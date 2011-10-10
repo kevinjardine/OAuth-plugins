e7768f0dda5dc1e6a96af72f8fa08e5704a9fd17
Kevin Jardine <kevinjardine@yahoo.com>
2011-10-10 12:17:29 +0200

    cleaned up Elgg store code

--- oauth/vendors/oauth-php/library/store/OAuthStoreElgg.php
+++ oauth/vendors/oauth-php/library/store/OAuthStoreElgg.php
@@ -1,69 +1,33 @@
 <?php
 
 /**
- * Will be converted into OAuthStoreElgg in due course.
- * 
- * OAuthSession is a really *dirty* storage. It's useful for testing and may 
- * be enough for some very simple applications, but it's not recommended for
- * production use.
- * 
- * @version $Id: OAuthStoreSession.php 153 2010-08-30 21:25:58Z brunobg@corollarium.com $
- * @author BBG
- * 
- * The MIT License
- * 
- * Copyright (c) 2007-2008 Mediamatic Lab
- * 
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- * 
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
+ * OAuth Elgg store
+ *
+ * @package oauth
+ * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
+ * @author Kevin Jardine <kevin@radagast.biz>
+ * @copyright Radagast Solutions 2011
+ * @link http://radagast.biz/
+ *
  */
 
 require_once dirname(__FILE__) . '/OAuthStoreAbstract.class.php';
 
 class OAuthStoreElgg extends OAuthStoreAbstract
 {
-	
 	protected $current_server_uri = '';
+	protected $current_scope = array();
 	
 	// an Elgg store needs nothing to set up
-	public function __construct( $current_server_uri = '' )	{
-		if ($current_server_uri) {
+	// but optionally can take a server_uri and scope array
+	// these can also be added later using the setter functions
+	public function __construct( $current_server_uri = null, $current_scope = null )	{
+		if (isset($current_server_uri)) {
 			$this->current_server_uri;
 		}
-		/*if (!session_id()) {
-			session_start();
+		if (isset($current_scope)) {
+			$this->current_scope = $current_scope;
 		}
-		if(isset($options['consumer_key']) && isset($options['consumer_secret']))
-		{
-			$this->session = &$_SESSION['oauth_' . $options['consumer_key']];
-			$this->session['consumer_key'] = $options['consumer_key'];
-			$this->session['consumer_secret'] = $options['consumer_secret'];
-			$this->session['signature_methods'] = array('HMAC-SHA1');
-			$this->session['server_uri'] = $options['server_uri']; 
-			$this->session['request_token_uri'] = $options['request_token_uri'];
-			$this->session['authorize_uri'] = $options['authorize_uri'];
-			$this->session['access_token_uri'] = $options['access_token_uri']; 
-			
-		}
-		else
-		{
-			throw new OAuthException2("OAuthStoreSession needs consumer_token and consumer_secret");
-		}*/
 	}
 
 	public function getSecretsForVerify ( $consumer_key, $token, $token_type = 'access' ) { throw new OAuthException2("OAuthStoreElgg doesn't support " . __METHOD__); }
@@ -90,9 +54,8 @@ class OAuthStoreElgg extends OAuthStoreAbstract
 	 * @return array
 	 */
 	public function getSecretsForSignature ( $uri, $user_id ) {
-		// always returns the most recent access token for this uri and user_id,
-		// if available
-		// but should it?
+		// always returns the most recent unexpired access token for this uri and user_id,
+		// if available, but should it?
 		if ($this->current_server_uri) {
 			$server = $this->getServerForUri($this->current_server_uri,$user_id);
 			if (is_array($server->signature_methods)) {
@@ -100,21 +63,10 @@ class OAuthStoreElgg extends OAuthStoreAbstract
 			} else {
 				$signature_methods =  array($server->signature_methods);
 			}
-			$options = array (
-				'type' => 'object',
-				'subtype' => 'oauth_server_token',
-				'owner_guid' => $user_id,
-				'metadata_name_value_pairs' => array(
-					array('name'=>'server_uri','value'=>sanitize_string($this->current_server_uri)),
-					//array('name'=>'token_ttl','value'=>time(),'operand'=>'>='),
-					array('name'=>'token_type','value'=>'access')
-				),
-				'order_by' => 'e.guid DESC',
-				'limit' => 1,			
-			);
-			$entities = elgg_get_entities_from_metadata($options);
-			if ($entities) {
-				$token = $entities[0];
+			
+			$tokens = $this->getServerTokens($this->current_server_uri,'access',$user_id,$this->current_scope);
+			if ($tokens) {
+				$token = $tokens[0];
 				return array(
 					'consumer_key' 		=> 	$server->consumer_key,
 					'consumer_secret' 	=> 	$server->consumer_secret,
@@ -132,42 +84,15 @@ class OAuthStoreElgg extends OAuthStoreAbstract
 		} else {
 			throw new OAuthException2('No server URI defined');
 		}
-		//return $this->session;
 	}
+	
+	// currently ignores the $token and $name parameters but should perhaps use them if defined
 
 	public function getServerTokenSecrets ( $consumer_key, $token, $token_type, $user_id, $name = '') { 
-		/*if ($consumer_key != $this->session['consumer_key']) {
-			return array();
-		} 
-		return array(
-			'consumer_key' => $consumer_key,
-			'consumer_secret' => $this->session['consumer_secret'],
-			'token' => $token,
-			'token_secret' => $this->session['token_secret'],
-			'token_name' => $name,
-			'signature_methods' => $this->session['signature_methods'],
-			'server_uri' => $this->session['server_uri'],
-			'request_token_uri' => $this->session['request_token_uri'],
-			'authorize_uri' => $this->session['authorize_uri'],
-			'access_token_uri' => $this->session['access_token_uri'],
-			'token_ttl' => 3600,
-		);*/
 		if ($this->current_server_uri) {
-			$options = array (
-				'type' => 'object',
-				'subtype' => 'oauth_server_token',
-				'owner_guid' => $user_id,
-				'metadata_name_value_pairs' => array(
-					array('name'=>'server_uri','value'=>sanitize_string($this->current_server_uri)),
-					//array('name'=>'token_ttl','value'=>time(),'operand'=>'>='),
-					array('name'=>'token_type','value'=>sanitize_string($token_type)),
-				),
-				'order_by' => 'e.guid DESC',
-				'limit' => 1,			
-			);
-			$entities = elgg_get_entities_from_metadata($options);
-			if ($entities) {
-				$token = $entities[0];
+			$tokens = $this->getServerTokens($this->current_server_uri,$token_type,$user_id,$this->current_scope);
+			if ($tokens) {
+				$token = $tokens[0];
 				$server = $this->getServerForUri ( $this->current_server_uri, $user_id );
 				if ($server) {
 					if (is_array($server->signature_methods)) {
@@ -188,13 +113,15 @@ class OAuthStoreElgg extends OAuthStoreAbstract
 						'access_token_uri' => $server->access_token_uri,
 						'token_ttl' => $token->token_ttl-time(),
 					);
-					//print_r($result);
 					return $result;
 				}
 			}
 		}
 		return array();
 	}
+	
+	// TODO consider making the server description the parent object to the tokens
+	// rather than using server_uri
 	
 	public function addServerToken ( $consumer_key, $token_type, $token, $token_secret, $user_id, $options = array() ) {
 		$token_obj = new ElggObject();
@@ -213,15 +140,16 @@ class OAuthStoreElgg extends OAuthStoreAbstract
 		if (isset($options['token_ttl']) && is_numeric($options['token_ttl'])) {
 			$token_obj->token_ttl = time()+$options['token_ttl'];
 		} else {
-			// make it last forever
-			$token_obj->token_ttl = 1000000000;			
+			// make it last for ten years
+			$token_obj->token_ttl = time()+60*60*24*365*10;			
 		}
 		if (isset($options['name'])) {
 			$token_obj->name = $options['name'];
 		}
-		if (isset($options['token_ttl']) && is_numeric($options['token_ttl'])) {
-			$token_obj->token_ttl = time()+$options['token_ttl'];
-		}
+		
+		if (isset($this->current_scope)) {
+			$token_obj->scope = $this->current_scope;
+		}		
 		
 		$token_obj->save();
 	}
@@ -283,7 +211,6 @@ class OAuthStoreElgg extends OAuthStoreAbstract
 		if ($user_guid) {
 			$options['owner_guid'] = $user_id;
 		}
-		error_log("Attempting to delete token $token");
 		$entities = elgg_get_entities_from_metadata($options);
 		foreach($entities as $e) {
 			$e->delete();
@@ -346,12 +273,23 @@ class OAuthStoreElgg extends OAuthStoreAbstract
 
 	public function checkServerNonce ( $consumer_key, $token, $timestamp, $nonce ) { throw new OAuthException2("OAuthStoreElgg doesn't support " . __METHOD__); }
 	
-	public function addLog ( $keys, $received, $sent, $base_string, $notes, $user_id = null ) { error_log(print_r(array($keys, $received, $sent, $base_string, $notes, $user_id),TRUE)); }
+	public function addLog ( $keys, $received, $sent, $base_string, $notes, $user_id = null ) {
+		// crude method to get this done for now
+		// TODO: should there be oauth_log_entry objects?
+		error_log(print_r(array(
+			'keys'			=>	$keys,
+			'received'		=>	$received, 
+			'sent'			=>	$sent, 
+			'base_string'	=>	$base_string, 
+			'notes'			=>	$notes, 
+			'user_guid'		=>	$user_id
+		),TRUE)); 
+	}
 	public function listLog ( $options, $user_id ) { throw new OAuthException2("OAuthStoreElgg doesn't support " . __METHOD__); }
 	
 	public function install () { throw new OAuthException2("OAuthStoreElgg doesn't support " . __METHOD__); }
 
-	// extra getter and setter for current_server_uri
+	// extra getters and setters for current_server_uri and current_scope
 	
 	public function getCurrentServerURI() {
 		return $this->current_server_uri;
@@ -359,6 +297,42 @@ class OAuthStoreElgg extends OAuthStoreAbstract
 	
 	public function setCurrentServerURI($current_server_uri) {
 		$this->current_server_uri = $current_server_uri;
+	}
+	
+	public function getCurrentScope() {
+		return $this->current_scope;
+	}
+	
+	public function setCurrentScope($current_scope) {
+		$this->current_scope = $current_scope;
+	}
+	
+	// TODO: putting four metadata constraints on this query is really inefficient
+	// try to make this more efficient (eg. make tokens contained by a server parent object)
+	
+	public function getServerTokens($server_uri,$token_type,$user_id,$scope = null,$limit=1) {
+		$options = array (
+			'type' => 'object',
+			'subtype' => 'oauth_server_token',
+			'owner_guid' => $user_id,
+			'metadata_name_value_pairs' => array(
+				array('name'=>'server_uri','value'=>sanitize_string($server_uri)),
+				array('name'=>'token_ttl','value'=>time(),'operand'=>'>='),
+				array('name'=>'token_type','value'=>sanitize_string($token_type)),
+			),
+			'order_by' => 'e.guid DESC',
+			'limit' => $limit,			
+		);
+		if ($scope) {
+			$scopes = array();
+			foreach($this->current_scope as $s) {
+				$scopes[] = sanitize_string($s);
+			}
+			if ($scopes) {
+				$options['metadata_name_value_pairs'][] = array('name'=>'scope','value'=>$scopes,'operand'=>'IN');
+			}
+		}
+		return elgg_get_entities_from_metadata($options);
 	}
 }
 

0f6c1f36cbaa75c3274948e4cbbc68e4fa92da7e
Kevin Jardine <kevinjardine@yahoo.com>
2011-10-10 16:27:01 +0200

    made forward optional and added POST variation

--- oauth/models/model.php
+++ oauth/models/model.php
@@ -40,7 +40,43 @@ function oauth_handle_request_with_redirects($url,$user_guid,$store,$server,$rea
 			// for another one
 			$secrets_array = $store->getServerTokenSecrets($server->consumer_key, '', 'access', $user_guid);
 			$store->deleteServerToken($server->consumer_key,$secrets_array['token'],$user_guid);
-			forward($reauthorize_forward);
+			if($reauthorize_forward) {
+				forward($reauthorize_forward);
+				exit;
+			} else {
+				return FALSE;
+			}
+		}
+	}
+	
+	return $result;
+}
+
+// handle potential redirects
+// need to do it this way because each redirect needs to be re-signed
+
+function oauth_handle_post_with_redirects($url,$params,$body,$user_guid,$store,$server,$reauthorize_forward) {
+	for ($i=0; $i<5;$i++) {
+		$request = new OAuthRequester($url, 'POST', $params, $body);
+		try {
+			$result = $request->doRequest($user_guid);
+			if ($result['code'] == 302) {
+				$url = $result['headers']['location'];
+			} else {
+				break;
+			}
+		} catch(OAuthException2 $e) {
+			// so far as I can see, if an exception is triggered at this stage, 
+			// the user must have revoked the access token, so delete the one we have and ask 
+			// for another one
+			$secrets_array = $store->getServerTokenSecrets($server->consumer_key, '', 'access', $user_guid);
+			$store->deleteServerToken($server->consumer_key,$secrets_array['token'],$user_guid);
+			if($reauthorize_forward) {
+				forward($reauthorize_forward);
+				exit;
+			} else {
+				return FALSE;
+			}
 			exit;
 		}
 	}
